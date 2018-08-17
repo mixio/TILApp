776694d78e8e8f6aeaa55f1f99f1e4b442bd1480
dev <dev@mixio.com>
2018-08-17 15:38:15 +0200

    JJTools

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
 
-        .package(url: "https://github.com/mixio/jjtools.git", from: "0.0.8"),
+        .package(url: "https://github.com/mixio/jjtools.git", from: "0.0.9"),
 
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
 
